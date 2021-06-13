e914952c0edeeb142dbd1391a0857a3e7ec6f648
Pattarawan-012 <pattarawan2001@gmail.com>
2021-06-13 17:00:33 +0700

    ข้อสอบ ข้อ 6

---
+++ FinalNo.6/FinalNo.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalNo._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
