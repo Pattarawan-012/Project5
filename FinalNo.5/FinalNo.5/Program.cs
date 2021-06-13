ed92467f48d11d40363ffae9631e5f44ac7ce2aa
Pattarawan-012 <pattarawan2001@gmail.com>
2021-06-13 17:00:10 +0700

    ข้อสอบ ข้อ 5

---
+++ FinalNo.5/FinalNo.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalNo._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
